b78aac88d81bdd5179e3a251d031dd6546f47c86
Denisa Plačková <dejna.plackova@gmail.com>
2022-04-24 22:57:50 +0200

    Napojení JS, základ hry

---
+++ index.js
@@ -0,0 +1,23 @@
+let player = 'circle';
+
+const square = document.querySelector('#tah');
+
+const move = (event) => {
+  if (player === 'circle') {
+    event.target.disabled = true;
+    event.target.classList.add('board__field--circle');
+    player = 'cross';
+    square.src = 'cross.svg';
+  } else {
+    event.target.disabled = true;
+    player = 'circle';
+    ev.target.classList.add('board__field--cross');
+    square.src = 'circle.svg';
+  }
+  console.log(event);
+};
+
+const buttons = document.querySelectorAll('.hraci-pole button');
+for (let i = 0; i < buttons.length; i += 1) {
+  buttons[i].addEventListener('click', move);
+}
