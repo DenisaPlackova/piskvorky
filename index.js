8e8cdbd1a8744db046b691742b8b057f6c156056
Denisa Plačková <dejna.plackova@gmail.com>
2022-05-03 11:44:56 +0200

    oprava hrací ikony

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 let player = 'circle';
 
 const square = document.querySelector('.tah');
+const buttons = document.querySelectorAll('.hraci-pole button');
 
 const move = (event) => {
   if (player === 'circle') {
     event.target.disabled = true;
     event.target.classList.add('board__field--circle');
     player = 'cross';
-    square.src = 'cross.svg';
+    square.classList.add('board__field--cross');
+    square.classList.remove('board__field--circle');
   } else {
     event.target.disabled = true;
     player = 'circle';
     event.target.classList.add('board__field--cross');
-    square.src = 'circle.svg';
+    square.classList.add('board__field--circle');
+    square.classList.remove('board__field--cross');
   }
   console.log(event);
 };
 
-const buttons = document.querySelectorAll('.hraci-pole button');
 for (let i = 0; i < buttons.length; i += 1) {
   buttons[i].addEventListener('click', move);
 }
